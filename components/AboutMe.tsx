ca74d65deaddaf5431b7b4d98dcc0f7c8c843bb8
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-06 00:41:49 +0530

    Update AboutMe component to adjust text size for better readability

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -9,7 +9,7 @@ const AboutMe = () => {
   return (
     <section>
       <Title text="About Me." />
-      <p className="flex flex-col gap-2 md:text-xl dark:text-zinc-400">
+      <p className="flex flex-col gap-2 md:text-lg dark:text-zinc-400">
         <span>
           I&apos;ve been coding for the past 3 years, mainly focusing on web
           development and building scalable backend systems.
