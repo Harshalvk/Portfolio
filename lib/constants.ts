4fa3c9ec0f9b0114d7fe426b918b50635e9ce022
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-06 00:41:25 +0530

    Add project constants for CodePost including description, image source, and technology badges

---
+++ lib/constants.ts
@@ -0,0 +1,35 @@
+export const projects = [
+  {
+    title: "CodePost",
+    description:
+      "A minimal, distraction-free platform for sharing ideas, stories, and thoughts built for clarity, speed, and effortless publishing.",
+    imageSrc: "/codepost.png",
+    techBadge: [
+      {
+        logo: "/react_light.svg",
+        alt: "react-logo",
+        name: "React",
+      },
+      {
+        logo: "/prisma.svg",
+        alt: "prisma-logo",
+        name: "Prisma",
+      },
+      {
+        logo: "/shadcn-ui_dark.svg",
+        alt: "shadcn-logo",
+        name: "Shadcn UI",
+      },
+      {
+        logo: "/tailwindcss.svg",
+        alt: "tailwindcss-logo",
+        name: "Tailwind CSS",
+      },
+      {
+        logo: "/nextjs.svg",
+        alt: "nextjs-logo",
+        name: "NextJS",
+      },
+    ],
+  },
+];
