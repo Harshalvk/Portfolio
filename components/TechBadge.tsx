8d42ce90e7450670b5575b42fd929e2c3e8ba438
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-15 22:26:42 +0530

    Refactor TechBadge component to improve styling and remove unused imports

--- components/TechBadge.tsx
+++ components/TechBadge.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import React from "react";
-import { AspectRatio } from "./ui/aspect-ratio";
-import { Button } from "./ui/button";
 
 const TechBadge = ({
   imageSrc,
@@ -13,8 +11,8 @@ const TechBadge = ({
   alt: string;
 }) => {
   return (
-    <div className="border rounded-sm flex items-center gap-1 w-fit py-1 px-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 hover:border-zinc-500/40 transition-all duration-200 cursor-default">
-      <Image src={`${imageSrc}`} alt={`${alt}`} width={20} height={20} />
+    <div className="h-8 border rounded-sm flex items-center gap-1 w-fit py-1 px-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 hover:border-zinc-500/40 transition-all duration-200 cursor-default">
+      <Image src={`${imageSrc}`} alt={`${alt}`} width={20} height={20} className="h-4 sm:h-6 rounded-sm" />
       <p className="text-sm font-medium tracking-wide cursor-default">{name}</p>
     </div>
   );
