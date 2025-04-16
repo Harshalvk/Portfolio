20184200bcae6781d2c821eb501b581937b6e25c
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-16 16:04:05 +0530

    Refactor TechBadge component: replace image props with Logo prop for improved flexibility

--- components/TechBadge.tsx
+++ components/TechBadge.tsx
@@ -2,17 +2,15 @@ import Image from "next/image";
 import React from "react";
 
 const TechBadge = ({
-  imageSrc,
   name,
-  alt,
+  Logo,
 }: {
-  imageSrc: string;
-  name: string;
-  alt: string;
+  name?: string;
+  Logo?: React.ReactNode;
 }) => {
   return (
     <div className="h-8 border rounded-sm flex items-center gap-1 w-fit py-1 px-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 hover:border-zinc-500/40 transition-all duration-200 cursor-default">
-      <Image src={`${imageSrc}`} alt={`${alt}`} width={20} height={20} className="h-4 sm:h-6 rounded-sm" />
+      {Logo && <div className="h-5 w-5">{Logo}</div>}
       <p className="text-sm font-medium tracking-wide cursor-default">{name}</p>
     </div>
   );
