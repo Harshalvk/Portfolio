a6963e4fcdaaabfb4bbf5a1acf7c35f9439bbadc
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-16 16:04:13 +0530

    Refactor Title component: update heading styles for improved responsiveness and visual hierarchy

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Title = ({ text }: { text: string }) => {
   return (
-    <h1 className="md:text-xl font-semibold tracking-tight my-3">{text}</h1>
+    <h1 className="mb-3 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight">
+      {text}
+    </h1>
   );
 };
 
