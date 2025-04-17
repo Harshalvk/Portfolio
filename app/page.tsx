25cfce0a835afb9533a88a482412d583dcddb4ba
Harshalvk <harshalvkhobragade@gmail.com>
2025-04-17 15:21:37 +0530

    feat: add ContactMe section and Footer with separator to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import AboutMe from "@/components/AboutMe";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
+import ContactMe from "@/components/Sections/ContactMe";
+import Footer from "@/components/Sections/Footer";
 import MyTechnologies from "@/components/Sections/MyTechnologies";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
@@ -10,6 +13,9 @@ export default function Home() {
       <AboutMe />
       <Projects />
       <MyTechnologies />
+      <ContactMe />
+      <Separator className="my-5"/>
+      <Footer />
     </section>
   );
 }
