d9cd73e5493828085e934ad3838a8424bc4c80d9
Harshalvk <harshalvkhobragade@gmail.com>
2025-05-02 00:14:02 +0530

    feat: restructure AppProvider to include QueryClientProvider and Toaster component

--- components/AppProvider.tsx
+++ components/AppProvider.tsx
@@ -1,19 +1,28 @@
+"use client";
+
 import React from "react";
 import { ThemeProvider } from "./Providers/ThemeProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./ui/sonner";
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <ThemeProvider
-        attribute={"class"}
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          attribute={"class"}
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Toaster />
+          {children}
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
-};  
+};
 
 export default AppProvider;
